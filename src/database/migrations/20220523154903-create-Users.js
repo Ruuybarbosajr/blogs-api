1253c4cac850afec300e782d14d2c1ed5a6dd001
Ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-05-23 13:31:27 -0300

    create migration table users

---
+++ src/database/migrations/20220523154903-create-Users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        // field: 'display_name'
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users')
+  }
+};
